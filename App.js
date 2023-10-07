ef967a050cc4103f13f2cf7253bde9265d638a53
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-10-07 15:57:57 +0000

    flight price options list

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import SearchForm from "./src/components/SerchForm";
-import {LinearGradient} from 'expo-linear-gradient'
+import { LinearGradient } from "expo-linear-gradient";
+import data from "./data.json";
+import FlightOptionItem from "./src/components/FlightOptionItem";
+
+const option1 = data[3];
 
 export default function App() {
   return (
-    <LinearGradient colors={['white','#E0EFFF']} style={styles.container}>
+    <LinearGradient colors={["white", "#E0EFFF"]} style={styles.container}>
       <SafeAreaView>
         <SearchForm />
+        {/*  <FlightOptionItem flight={option1}/> */}
+        <FlatList
+          renderItem={({ item }) => <FlightOptionItem flight={item} />}
+          data={data}
+          showsVerticalScrollIndicator={false}
+        />
       </SafeAreaView>
 
       <StatusBar style="auto" />
