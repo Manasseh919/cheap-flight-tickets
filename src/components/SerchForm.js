29fe9a0e25419983ab7441df630775b46eab8995
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-10-07 15:41:50 +0000

    date picker component done

--- src/components/SerchForm.js
+++ src/components/SerchForm.js
@@ -1,12 +1,16 @@
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Feather } from "@expo/vector-icons";
 
 const SearchForm = () => {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+  const [departDate, setDepartDate] = useState(new Date());
+  const [returnDate, setReturnDate] = useState(new Date());
 
   const onSearchPressed = () => {
-    console.log("searching for: ");
+    console.log({from,to,departDate,returnDate});
   };
   return (
     <View style={styles.card}>
@@ -23,6 +27,21 @@ const SearchForm = () => {
         value={to}
         onChangeText={setTo}
       />
+
+      <View style={styles.datePicker}>
+        <Feather name="calendar" size={26} color="gray" />
+        <DateTimePicker
+          value={departDate}
+          onChange={(event, date) => setDepartDate(date || new Date())}
+          minimumDate={new Date()}
+        />
+        <Text style={{fontSize:20,color:'gainsboro',marginLeft:10}}>|</Text>
+        <DateTimePicker
+          value={returnDate}
+          onChange={(event, date) => setReturnDate(date || new Date())}
+          minimumDate={departDate}
+        />
+      </View>
 
       <Button title="Search" onPress={onSearchPressed} />
     </View>
@@ -60,5 +79,14 @@ const styles = StyleSheet.create({
     padding: 10,
     marginVertical: 5,
     borderRadius: 10,
+  },
+  datePicker: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    padding: 5,
+    borderRadius: 5,
+    marginVertical: 5,
+    flexDirection: "row",
+    alignItems:'center'
   },
 });
