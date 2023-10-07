0d7710c82fb535455eeed70f1aaa6bf9551a9fd7
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-10-07 15:19:23 +0000

    search form ui

---
+++ src/components/SerchForm.js
@@ -0,0 +1,51 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+
+const SearchForm = () => {
+  const onSearchPressed = () => {
+    console.log("searching for: ");
+  };
+  return (
+    <View style={styles.card}>
+      <Text style={styles.title}>Search the best prices for your trip</Text>
+      <TextInput style={styles.input} placeholder="From" />
+      <TextInput style={styles.input} placeholder="To" />
+
+      <Button title="Search" onPress={onSearchPressed} />
+    </View>
+  );
+};
+
+export default SearchForm;
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    margin: 10,
+    padding: 15,
+    borderRadius: 10,
+
+    //shadows react native shadow genertor
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  title: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+    marginVertical: 15,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gainsboro",
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+});
